9bc200d359e265778af817979a9def1c1dfa40e0
Chelbsik <Chelbsik@gmail.com>
2021-03-09 23:20:38 +0300

    task02 | financial_guru.sh | modify according to alexander request; get rid of copy-paste, implement function

--- task02/README.md
+++ task02/README.md
@@ -6,6 +6,13 @@ financial_guru.sh
 - Cuts out quotes for every March from 2015.
 - Calculates volatility for every year March.
 - Finally, chooses and prints year with least volatile March.
+
+UPDATED 09.03.2021:
+
+- Now script can take year gap as first two parameters (./financial_guru.sh "starting year" "ending year")
+- And month as third parameter (./financial_guru.sh "starting year" "ending year" "Mon")
+- For correct work all three parameters must be specified
+- Month must be in abbreviate format (three first letters, first one is capital: "Jan", "Jul", "Sep", etc.)
 -----------------------------------------------------------------------------
 no_pattern_matching.sh
 
--- task02/financial_guru.sh
+++ task02/financial_guru.sh
@@ -1,49 +1,97 @@
 #!/bin/bash
 
-DATE_VAL_TUPLE=$(jq '.prices[][0] |= ( . /1000 | strftime("%Y-%m-%d"))' quotes.json | 
+DATE_VAL_TUPLE=$(jq '.prices[][0] |= ( . /1000 | strftime("%Y-%b-%d"))' quotes.json | 
 jq -c '.prices[]')
 
-M15=$(echo "$DATE_VAL_TUPLE" | grep '2015-03')
-M16=$(echo "$DATE_VAL_TUPLE" | grep '2016-03')
-M17=$(echo "$DATE_VAL_TUPLE" | grep '2017-03')
-M18=$(echo "$DATE_VAL_TUPLE" | grep '2018-03')
-M19=$(echo "$DATE_VAL_TUPLE" | grep '2019-03')
-M20=$(echo "$DATE_VAL_TUPLE" | grep '2020-03')
+VOL () {
+[[ $1 < '2015' ]] && echo "Error: year must start from 2015" > /dev/stderr && exit 1
+[[ $2 > '2021' ]] && echo "Error: year must end at 2021" > /dev/stderr && exit 1
 
-M15_MIN=$(echo "$M15" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s min)
-M15_MAX=$(echo "$M15" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s max)
-M15_DIFF=$(awk "BEGIN {print $M15_MAX - $M15_MIN}")
-
-M16_MIN=$(echo "$M16" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s min)
-M16_MAX=$(echo "$M16" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s max)
-M16_DIFF=$(awk "BEGIN {print $M16_MAX - $M16_MIN}")
-
-M17_MIN=$(echo "$M17" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s min)
-M17_MAX=$(echo "$M17" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s max)
-M17_DIFF=$(awk "BEGIN {print $M17_MAX - $M17_MIN}")
-
-M18_MIN=$(echo "$M18" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s min)
-M18_MAX=$(echo "$M18" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s max)
-M18_DIFF=$(awk "BEGIN {print $M18_MAX - $M18_MIN}")
-
-M19_MIN=$(echo "$M19" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s min)
-M19_MAX=$(echo "$M19" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s max)
-M19_DIFF=$(awk "BEGIN {print $M19_MAX - $M19_MIN}")
-
-M20_MIN=$(echo "$M20" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s min)
-M20_MAX=$(echo "$M20" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s max)
-M20_DIFF=$(awk "BEGIN {print $M20_MAX - $M20_MIN}")
-
-DIF_STR=("$M15_DIFF $M16_DIFF $M17_DIFF $M18_DIFF $M19_DIFF $M20_DIFF")
-MIN_VOL=$(for x in $DIF_STR
+if [[ $2 == '2021' ]] && [[ $3 != 'Jan' ]] && [[ $3 != 'Feb' ]]
+then
+   echo "Error: no data after 02.2021"
+   exit 1
+fi
+for i in $(seq $1 $2)
 do
-   echo $x
-done | jq -s min)
+   MIN=$(echo "$DATE_VAL_TUPLE" | grep "$i"-"$3" | cut -d, -f2 | cut -d] -f1 | jq -s min)
+   MAX=$(echo "$DATE_VAL_TUPLE" | grep "$i"-"$3" | cut -d, -f2 | cut -d] -f1 | jq -s max)
+   DIFF=$(awk "BEGIN {print $MAX - $MIN} ")
+   echo $3 $i "volatility is" $DIFF > /dev/stderr
+   echo $3 $i "is the least volatile:" $DIFF
+done
+echo "=====================================" > /dev/stderr
 
-[ $MIN_VOL == $M15_DIFF ] && echo "2015 March was least volatile: $MIN_VOL"
-[ $MIN_VOL == $M16_DIFF ] && echo "2016 March was least volatile: $MIN_VOL"
-[ $MIN_VOL == $M17_DIFF ] && echo "2017 March was least volatile: $MIN_VOL"
-[ $MIN_VOL == $M18_DIFF ] && echo "2018 March was least volatile: $MIN_VOL"
-[ $MIN_VOL == $M19_DIFF ] && echo "2019 March was least volatile: $MIN_VOL"
-[ $MIN_VOL == $M20_DIFF ] && echo "2020 March was least volatile: $MIN_VOL"
+}
+echo "$(VOL $1 $2 $3)" | awk 'NR == 1 { j = $0; min = $7 } NR > 1 && $7 < min { j = $0; min = $7 } END { print j ; }'
+
+echo "=====================================" > /dev/stderr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# M15=$(echo "$DATE_VAL_TUPLE" | grep '2015-03')
+# M16=$(echo "$DATE_VAL_TUPLE" | grep '2016-03')
+# M17=$(echo "$DATE_VAL_TUPLE" | grep '2017-03')
+# M18=$(echo "$DATE_VAL_TUPLE" | grep '2018-03')
+# M19=$(echo "$DATE_VAL_TUPLE" | grep '2019-03')
+# M20=$(echo "$DATE_VAL_TUPLE" | grep '2020-03')
+
+# M15_MIN=$(echo "$M15" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s min)
+# M15_MAX=$(echo "$M15" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s max)
+# M15_DIFF=$(awk "BEGIN {print $M15_MAX - $M15_MIN}")
+
+# M16_MIN=$(echo "$M16" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s min)
+# M16_MAX=$(echo "$M16" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s max)
+# M16_DIFF=$(awk "BEGIN {print $M16_MAX - $M16_MIN}")
+
+# M17_MIN=$(echo "$M17" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s min)
+# M17_MAX=$(echo "$M17" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s max)
+# M17_DIFF=$(awk "BEGIN {print $M17_MAX - $M17_MIN}")
+
+# M18_MIN=$(echo "$M18" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s min)
+# M18_MAX=$(echo "$M18" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s max)
+# M18_DIFF=$(awk "BEGIN {print $M18_MAX - $M18_MIN}")
+
+# M19_MIN=$(echo "$M19" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s min)
+# M19_MAX=$(echo "$M19" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s max)
+# M19_DIFF=$(awk "BEGIN {print $M19_MAX - $M19_MIN}")
+
+# M20_MIN=$(echo "$M20" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s min)
+# M20_MAX=$(echo "$M20" | cut -d ',' -f2 | cut -d ']' -f1 | sort | jq -s max)
+# M20_DIFF=$(awk "BEGIN {print $M20_MAX - $M20_MIN}")
+
+# DIF_STR=("$M15_DIFF $M16_DIFF $M17_DIFF $M18_DIFF $M19_DIFF $M20_DIFF")
+# MIN_VOL=$(for x in $DIF_STR
+# do
+#    echo $x
+# done | jq -s min)
+
+# [ $MIN_VOL == $M15_DIFF ] && echo "2015 March was least volatile: $MIN_VOL"
+# [ $MIN_VOL == $M16_DIFF ] && echo "2016 March was least volatile: $MIN_VOL"
+# [ $MIN_VOL == $M17_DIFF ] && echo "2017 March was least volatile: $MIN_VOL"
+# [ $MIN_VOL == $M18_DIFF ] && echo "2018 March was least volatile: $MIN_VOL"
+# [ $MIN_VOL == $M19_DIFF ] && echo "2019 March was least volatile: $MIN_VOL"
+# [ $MIN_VOL == $M20_DIFF ] && echo "2020 March was least volatile: $MIN_VOL"
 
