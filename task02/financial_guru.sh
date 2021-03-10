02c5957823b2dfdd2faa9379b80baa886e408867
Chelbsik <Chelbsik@gmail.com>
2021-03-10 19:29:00 +0300

    task02 | financial_guru.sh | add default meanings, changed regular echo output (stderr -> tty)

--- task02/financial_guru.sh
+++ task02/financial_guru.sh
@@ -4,28 +4,34 @@ DATE_VAL_TUPLE=$(jq '.prices[][0] |= ( . /1000 | strftime("%Y-%b-%d"))' quotes.j
 jq -c '.prices[]')
 
 VOL () {
-[[ $1 < '2015' ]] && echo "Error: year must start from 2015" > /dev/stderr && exit 1
-[[ $2 > '2021' ]] && echo "Error: year must end at 2021" > /dev/stderr && exit 1
+# default values
+[ -z $1 ] && START_YEAR='2015' || START_YEAR=$1
+[ -z $2 ] && END_YEAR='2021' || END_YEAR=$2
+[ -z $3 ] && MONTH='Feb' || MONTH=$3
 
-if [[ $2 == '2021' ]] && [[ $3 != 'Jan' ]] && [[ $3 != 'Feb' ]]
+[[ $START_YEAR < '2015' ]] && echo "Error: year must start from 2015" > /dev/stderr && exit 1
+[[ $END_YEAR > '2021' ]] && echo "Error: year must end at 2021" > /dev/stderr && exit 1
+
+if [[ $END_YEAR == '2021' ]] && [[ $MONTH != 'Jan' ]] && [[ $MONTH != 'Feb' ]]
 then
-   echo "Error: no data after 02.2021"
+   echo "Error: no data after 02.2021" > /dev/stderr
    exit 1
 fi
-for i in $(seq $1 $2)
+
+for i in $(seq $START_YEAR $END_YEAR)
 do
-   MIN=$(echo "$DATE_VAL_TUPLE" | grep "$i"-"$3" | cut -d, -f2 | cut -d] -f1 | jq -s min)
-   MAX=$(echo "$DATE_VAL_TUPLE" | grep "$i"-"$3" | cut -d, -f2 | cut -d] -f1 | jq -s max)
-   DIFF=$(awk "BEGIN {print $MAX - $MIN} ")
-   echo $3 $i "volatility is" $DIFF > /dev/stderr
-   echo $3 $i "is the least volatile:" $DIFF
+   MIN=$(echo "$DATE_VAL_TUPLE" | grep "$i"-"$MONTH" | cut -d, -f2 | cut -d] -f1 | jq -s min)
+   MAX=$(echo "$DATE_VAL_TUPLE" | grep "$i"-"$MONTH" | cut -d, -f2 | cut -d] -f1 | jq -s max)
+   DIFF=$(awk "BEGIN {print ($MAX - $MIN)/2} ")
+   echo $MONTH $i "volatility is" $DIFF > /dev/tty
+   echo $MONTH $i "is the least volatile:" $DIFF
 done
-echo "=====================================" > /dev/stderr
+echo "======================================" > /dev/tty
 
 }
 echo "$(VOL $1 $2 $3)" | awk 'NR == 1 { j = $0; min = $7 } NR > 1 && $7 < min { j = $0; min = $7 } END { print j ; }'
 
-echo "=====================================" > /dev/stderr
+echo "======================================"
 
 
 
